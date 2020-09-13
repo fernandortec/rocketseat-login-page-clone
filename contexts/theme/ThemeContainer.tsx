1efec7b7aac79a83c312d2837ff8d2d4f35ea3db
fernandotec <fernandorfigueiredotec@gmail.com>
2020-09-13 13:18:48 -0300

    Initial commit

---
+++ contexts/theme/ThemeContainer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  ColorModeProvider,
+  CSSReset,
+  ThemeProvider as ChakraThemeProvider,
+} from '@chakra-ui/core';
+
+import theme from '../../styles/theme';
+
+import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
+const ThemeContainer: React.FC = ({ children }) => {
+  return (
+    <ChakraThemeProvider theme={theme}>
+      <ColorModeProvider value="dark">
+        <EmotionThemeProvider theme={theme}>
+          <CSSReset />
+          {children}
+        </EmotionThemeProvider>
+      </ColorModeProvider>
+    </ChakraThemeProvider>
+  );
+};
+
+export default ThemeContainer;
